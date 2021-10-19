220980942c348a9703c40d0816cb79d195b6c1d5
GabrielMendoncaF <92555850+GabrielMendoncaF@users.noreply.github.com>
2021-10-19 14:31:13 -0300

    Add files via upload
    Realiza uma função previamente estabelecida n vezes à partir de um valor inicial que é acrescido de 0.5 a cada interação.

---
+++ Funcao_x_a_x+2.py
@@ -0,0 +1,12 @@
+import math
+n = float(input("Digite a base: ")) #entrada do número de interações
+
+x_inicial = float(input("Digite o x inicial: ")) #entrada do x inicial
+
+def funcao (x):
+    return round((2*(pow(x,3)) + math.cos(x)+10),1) #funcao a ser executada arredondando o resultado para 1 casa decimal
+
+while n > 0:
+    print("f (" + str(x_inicial) + ") = " + str(funcao(x_inicial))) #resposta
+    n = n -1 #para executar a quantidade de interações necessária (n vezes)
+    x_inicial = x_inicial + 0.5  #adiciona 0.5 ao x inicial e
